cf3cf8e35969643870a6302c4237dcf80abbcb53
Alexander Schutz <alexander.schutz@textability.com>
2019-03-10 18:02:42 +0000

    initial state machine with linear transitions

---
+++ src/main/java/ie/textability/dialog/DialogAct.java
@@ -0,0 +1,12 @@
+package ie.textability.dialog;
+
+public enum DialogAct {
+    ESCALATION,
+    ANSWER_YES,
+    ANSWER_NO,
+    GREETING,
+    GOODBYE,
+    INTENT_EXPRESSED,
+    SLOT_EXPRESSED,
+    RESET
+}
